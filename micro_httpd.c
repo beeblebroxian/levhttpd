6ad1d90d87b5b4340470d718f45e0f141b887179
beeblebroxian <129529466+beeblebroxian@users.noreply.github.com>
2026-06-20 18:50:06 +0200

    Update micro_httpd.c

--- micro_httpd.c
+++ micro_httpd.c
@@ -1,318 +1,525 @@
-/* micro_httpd - really small HTTP server
-**
-** Copyright © 1999,2005 by Jef Poskanzer <jef@mail.acme.com>.
-** All rights reserved.
-**
-** Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions
-** are met:
-** 1. Redistributions of source code must retain the above copyright
-**    notice, this list of conditions and the following disclaimer.
-** 2. Redistributions in binary form must reproduce the above copyright
-**    notice, this list of conditions and the following disclaimer in the
-**    documentation and/or other materials provided with the distribution.
-** 
-** THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
-** ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-** IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-** ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
-** FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-** DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-** OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-** HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-** LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-** OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-** SUCH DAMAGE.
-*/
-
+/*
+ * mini_httpd standalone derivato da micro_httpd
+ * uso:
+ *
+ *   httpserver PORT DIRECTORY
+ *
+ * esempio:
+ *
+ *   httpserver 18192 /sdcard/Movies
+ *
+ */
 
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-#include <dirent.h>
 #include <ctype.h>
 #include <time.h>
+#include <fcntl.h>
+
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
 
 
-#define SERVER_NAME "micro_httpd"
-#define SERVER_URL "http://www.acme.com/software/micro_httpd/"
+#define SERVER_NAME "android_httpserver"
 #define PROTOCOL "HTTP/1.0"
 #define RFC1123FMT "%a, %d %b %Y %H:%M:%S GMT"
 
 
-/* Forwards. */
-static void file_details( char* dir, char* name );
-static void send_error( int status, char* title, char* extra_header, char* text );
-static void send_headers( int status, char* title, char* extra_header, char* mime_type, off_t length, time_t mod );
-static char* get_mime_type( char* name );
-static void strdecode( char* to, char* from );
-static int hexit( char c );
-static void strencode( char* to, size_t tosize, const char* from );
+static void send_error(
+    int fd,
+    int status,
+    const char *title,
+    const char *text
+);
+
+static void send_headers(
+    int fd,
+    int status,
+    const char *title,
+    const char *mime_type,
+    off_t length,
+    time_t mod
+);
+
+static char *get_mime_type(char *name);
 
 
-int
-main( int argc, char** argv )
-    {
-    char line[10000], method[10000], path[10000], protocol[10000], idx[20000], location[20000];
-    char* file;
-    size_t len;
-    int ich;
+/*
+ * invia un file sul socket
+ */
+static void send_file(
+    int fd,
+    const char *root,
+    char *name
+)
+{
+    char path[4096];
     struct stat sb;
-    FILE* fp;
-    struct dirent **dl;
-    int i, n;
+    int file;
+    char buf[8192];
+    ssize_t n;
 
-    if ( argc != 2 )
-	send_error( 500, "Internal Error", (char*) 0, "Config error - no dir specified." );
-    if ( chdir( argv[1] ) < 0 )
-	send_error( 500, "Internal Error", (char*) 0, "Config error - couldn't chdir()." );
-    if ( fgets( line, sizeof(line), stdin ) == (char*) 0 )
-	send_error( 400, "Bad Request", (char*) 0, "No request found." );
-    if ( sscanf( line, "%[^ ] %[^ ] %[^ ]", method, path, protocol ) != 3 )
-	send_error( 400, "Bad Request", (char*) 0, "Can't parse request." );
-    while ( fgets( line, sizeof(line), stdin ) != (char*) 0 )
-	{
-	if ( strcmp( line, "\n" ) == 0 || strcmp( line, "\r\n" ) == 0 )
-	    break;
-	}
-    if ( strcasecmp( method, "get" ) != 0 )
-	send_error( 501, "Not Implemented", (char*) 0, "That method is not implemented." );
-    if ( path[0] != '/' )
-	send_error( 400, "Bad Request", (char*) 0, "Bad filename." );
-    file = &(path[1]);
-    strdecode( file, file );
-    if ( file[0] == '\0' )
-	file = "./";
-    len = strlen( file );
-    if ( file[0] == '/' || strcmp( file, ".." ) == 0 || strncmp( file, "../", 3 ) == 0 || strstr( file, "/../" ) != (char*) 0 || strcmp( &(file[len-3]), "/.." ) == 0 )
-	send_error( 400, "Bad Request", (char*) 0, "Illegal filename." );
-    if ( stat( file, &sb ) < 0 )
-	send_error( 404, "Not Found", (char*) 0, "File not found." );
-    if ( S_ISDIR( sb.st_mode ) )
-	{
-	if ( file[len-1] != '/' )
-	    {
-	    (void) snprintf(
-		location, sizeof(location), "Location: %s/", path );
-	    send_error( 302, "Found", location, "Directories must end with a slash." );
-	    }
-	(void) snprintf( idx, sizeof(idx), "%sindex.html", file );
-	if ( stat( idx, &sb ) >= 0 )
-	    {
-	    file = idx;
-	    goto do_file;
-	    }
-	send_headers( 200, "Ok", (char*) 0, "text/html", -1, sb.st_mtime );
-	(void) printf( "\
-<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n\
-<html>\n\
-  <head>\n\
-    <meta http-equiv=\"Content-type\" content=\"text/html;charset=UTF-8\">\n\
-    <title>Index of %s</title>\n\
-  </head>\n\
-  <body bgcolor=\"#99cc99\">\n\
-    <h4>Index of %s</h4>\n\
-    <pre>\n", file, file );
-	n = scandir( file, &dl, NULL, alphasort );
-	if ( n < 0 )
-	    perror( "scandir" );
-	else
-	    for ( i = 0; i < n; ++i )
-		file_details( file, dl[i]->d_name );
-	(void) printf( "\
-    </pre>\n\
-    <hr>\n\
-    <address><a href=\"%s\">%s</a></address>\n\
-  </body>\n\
-</html>\n", SERVER_URL, SERVER_NAME );
-	}
-    else
-	{
-	do_file:
-	fp = fopen( file, "r" );
-	if ( fp == (FILE*) 0 )
-	    send_error( 403, "Forbidden", (char*) 0, "File is protected." );
-	send_headers( 200, "Ok", (char*) 0, get_mime_type( file ), sb.st_size, sb.st_mtime );
-	while ( ( ich = getc( fp ) ) != EOF )
-	    putchar( ich );
-	}
 
-    (void) fflush( stdout );
-    exit( 0 );
+    snprintf(
+        path,
+        sizeof(path),
+        "%s/%s",
+        root,
+        name
+    );
+
+
+    if (stat(path, &sb) < 0)
+    {
+        send_error(
+            fd,
+            404,
+            "Not Found",
+            "File not found"
+        );
+        return;
     }
 
 
-static void
-file_details( char* dir, char* name )
+    if (!S_ISREG(sb.st_mode))
     {
-    static char encoded_name[1000];
-    static char path[2000];
-    struct stat sb;
-    char timestr[16];
-
-    strencode( encoded_name, sizeof(encoded_name), name );
-    (void) snprintf( path, sizeof(path), "%s/%s", dir, name );
-    if ( lstat( path, &sb ) < 0 )
-	(void) printf( "<a href=\"%s\">%-32.32s</a>    ???\n", encoded_name, name );
-    else
-	{
-	(void) strftime( timestr, sizeof(timestr), "%d%b%Y %H:%M", localtime( &sb.st_mtime ) );
-	(void) printf( "<a href=\"%s\">%-32.32s</a>    %15s %14lld\n", encoded_name, name, timestr, (long long) sb.st_size );
-	}
+        send_error(
+            fd,
+            403,
+            "Forbidden",
+            "Not a file"
+        );
+        return;
     }
 
 
-static void
-send_error( int status, char* title, char* extra_header, char* text )
+    file = open(path, O_RDONLY);
+
+    if (file < 0)
     {
-    send_headers( status, title, extra_header, "text/html", -1, -1 );
-    (void) printf( "\
-<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n\
-<html>\n\
-  <head>\n\
-    <meta http-equiv=\"Content-type\" content=\"text/html;charset=UTF-8\">\n\
-    <title>%d %s</title>\n\
-  </head>\n\
-  <body bgcolor=\"#cc9999\">\n\
-    <h4>%d %s</h4>\n", status, title, status, title );
-    (void) printf( "%s\n", text );
-    (void) printf( "\
-    <hr>\n\
-    <address><a href=\"%s\">%s</a></address>\n\
-  </body>\n\
-</html>\n", SERVER_URL, SERVER_NAME );
-    (void) fflush( stdout );
-    exit( 1 );
+        send_error(
+            fd,
+            403,
+            "Forbidden",
+            "Cannot open file"
+        );
+        return;
     }
 
 
-static void
-send_headers( int status, char* title, char* extra_header, char* mime_type, off_t length, time_t mod )
+    send_headers(
+        fd,
+        200,
+        "OK",
+        get_mime_type(path),
+        sb.st_size,
+        sb.st_mtime
+    );
+
+
+    while ((n = read(file, buf, sizeof(buf))) > 0)
     {
+        ssize_t off = 0;
+
+        while (off < n)
+        {
+            ssize_t w = write(
+                fd,
+                buf + off,
+                n - off
+            );
+
+            if (w <= 0)
+                break;
+
+            off += w;
+        }
+    }
+
+
+    close(file);
+}
+
+
+
+/*
+ * gestisce una richiesta HTTP
+ */
+static void handle_client(
+    int fd,
+    const char *root
+)
+{
+    char request[8192];
+    char method[32];
+    char path[4096];
+
+
+    int n = read(
+        fd,
+        request,
+        sizeof(request)-1
+    );
+
+
+    if (n <= 0)
+        return;
+
+
+    request[n] = 0;
+
+
+    if (sscanf(
+            request,
+            "%31s %4095s",
+            method,
+            path
+        ) != 2)
+    {
+        send_error(
+            fd,
+            400,
+            "Bad Request",
+            "Invalid request"
+        );
+        return;
+    }
+
+
+    if (strcmp(method, "GET") != 0)
+    {
+        send_error(
+            fd,
+            501,
+            "Not Implemented",
+            "Only GET supported"
+        );
+        return;
+    }
+
+
+    if (path[0] == '/')
+        memmove(
+            path,
+            path + 1,
+            strlen(path)
+        );
+
+
+    /*
+     * evita ../
+     */
+    if (strstr(path, ".."))
+    {
+        send_error(
+            fd,
+            400,
+            "Bad Request",
+            "Invalid path"
+        );
+        return;
+    }
+
+
+    send_file(
+        fd,
+        root,
+        path
+    );
+
+static void send_error(
+    int fd,
+    int status,
+    const char *title,
+    const char *text
+)
+{
+    dprintf(
+        fd,
+        "%s %d %s\r\n"
+        "Server: %s\r\n"
+        "Connection: close\r\n"
+        "Content-Type: text/html\r\n"
+        "\r\n"
+        "<html><body>"
+        "<h1>%d %s</h1>"
+        "<p>%s</p>"
+        "</body></html>\r\n",
+        PROTOCOL,
+        status,
+        title,
+        SERVER_NAME,
+        status,
+        title,
+        text
+    );
+}
+
+
+
+static void send_headers(
+    int fd,
+    int status,
+    const char *title,
+    const char *mime_type,
+    off_t length,
+    time_t mod
+)
+{
+    char timebuf[128];
     time_t now;
-    char timebuf[100];
-
-    (void) printf( "%s %d %s\015\012", PROTOCOL, status, title );
-    (void) printf( "Server: %s\015\012", SERVER_NAME );
-    now = time( (time_t*) 0 );
-    (void) strftime( timebuf, sizeof(timebuf), RFC1123FMT, gmtime( &now ) );
-    (void) printf( "Date: %s\015\012", timebuf );
-    if ( extra_header != (char*) 0 )
-	(void) printf( "%s\015\012", extra_header );
-    if ( mime_type != (char*) 0 )
-	(void) printf( "Content-Type: %s\015\012", mime_type );
-    if ( length >= 0 )
-	(void) printf( "Content-Length: %lld\015\012", (long long) length );
-    if ( mod != (time_t) -1 )
-	{
-	(void) strftime( timebuf, sizeof(timebuf), RFC1123FMT, gmtime( &mod ) );
-	(void) printf( "Last-Modified: %s\015\012", timebuf );
-	}
-    (void) printf( "Connection: close\015\012" );
-    (void) printf( "\015\012" );
-    }
 
 
-static char*
-get_mime_type( char* name )
+    dprintf(
+        fd,
+        "%s %d %s\r\n",
+        PROTOCOL,
+        status,
+        title
+    );
+
+
+    dprintf(
+        fd,
+        "Server: %s\r\n",
+        SERVER_NAME
+    );
+
+
+    now = time(NULL);
+
+    strftime(
+        timebuf,
+        sizeof(timebuf),
+        RFC1123FMT,
+        gmtime(&now)
+    );
+
+    dprintf(
+        fd,
+        "Date: %s\r\n",
+        timebuf
+    );
+
+
+    if (mime_type)
+        dprintf(
+            fd,
+            "Content-Type: %s\r\n",
+            mime_type
+        );
+
+
+    if (length >= 0)
+        dprintf(
+            fd,
+            "Content-Length: %lld\r\n",
+            (long long)length
+        );
+
+
+    if (mod != (time_t)-1)
     {
-    char* dot;
+        strftime(
+            timebuf,
+            sizeof(timebuf),
+            RFC1123FMT,
+            gmtime(&mod)
+        );
 
-    dot = strrchr( name, '.' );
-    if ( dot == (char*) 0 )
-	return "text/plain; charset=UTF-8";
-    if ( strcmp( dot, ".html" ) == 0 || strcmp( dot, ".htm" ) == 0 )
-	return "text/html; charset=UTF-8";
-    if ( strcmp( dot, ".xhtml" ) == 0 || strcmp( dot, ".xht" ) == 0 )
-	return "application/xhtml+xml; charset=UTF-8";
-    if ( strcmp( dot, ".jpg" ) == 0 || strcmp( dot, ".jpeg" ) == 0 )
-	return "image/jpeg";
-    if ( strcmp( dot, ".gif" ) == 0 )
-	return "image/gif";
-    if ( strcmp( dot, ".png" ) == 0 )
-	return "image/png";
-    if ( strcmp( dot, ".css" ) == 0 )
-	return "text/css";
-    if ( strcmp( dot, ".xml" ) == 0 || strcmp( dot, ".xsl" ) == 0 )
-	return "text/xml; charset=UTF-8";
-    if ( strcmp( dot, ".au" ) == 0 )
-	return "audio/basic";
-    if ( strcmp( dot, ".wav" ) == 0 )
-	return "audio/wav";
-    if ( strcmp( dot, ".avi" ) == 0 )
-	return "video/x-msvideo";
-    if ( strcmp( dot, ".swf" ) == 0 )
-	return "application/x-shockwave-flash";
-    if ( strcmp( dot, ".mov" ) == 0 || strcmp( dot, ".qt" ) == 0 )
-	return "video/quicktime";
-    if ( strcmp( dot, ".mpeg" ) == 0 || strcmp( dot, ".mpe" ) == 0 )
-	return "video/mpeg";
-    if ( strcmp( dot, ".vrml" ) == 0 || strcmp( dot, ".wrl" ) == 0 )
-	return "model/vrml";
-    if ( strcmp( dot, ".midi" ) == 0 || strcmp( dot, ".mid" ) == 0 )
-	return "audio/midi";
-    if ( strcmp( dot, ".mp3" ) == 0 )
-	return "audio/mpeg";
-    if ( strcmp( dot, ".ogg" ) == 0 )
-	return "application/ogg";
-    if ( strcmp( dot, ".pac" ) == 0 )
-	return "application/x-ns-proxy-autoconfig";
-    return "text/plain; charset=UTF-8";
+        dprintf(
+            fd,
+            "Last-Modified: %s\r\n",
+            timebuf
+        );
     }
 
 
-static void
-strdecode( char* to, char* from )
+    dprintf(
+        fd,
+        "Connection: close\r\n"
+        "\r\n"
+    );
+}
+
+
+
+static char *get_mime_type(char *name)
+{
+    char *dot = strrchr(name, '.');
+
+
+    if (!dot)
+        return "application/octet-stream";
+
+
+    if (!strcasecmp(dot, ".mp4"))
+        return "video/mp4";
+
+
+    if (!strcasecmp(dot, ".mkv"))
+        return "video/x-matroska";
+
+
+    if (!strcasecmp(dot, ".avi"))
+        return "video/x-msvideo";
+
+
+    if (!strcasecmp(dot, ".jpg") ||
+        !strcasecmp(dot, ".jpeg"))
+        return "image/jpeg";
+
+
+    if (!strcasecmp(dot, ".png"))
+        return "image/png";
+
+
+    if (!strcasecmp(dot, ".html") ||
+        !strcasecmp(dot, ".htm"))
+        return "text/html";
+
+
+    return "application/octet-stream";
+}
+
+
+
+
+int main(
+    int argc,
+    char **argv
+)
+{
+    int port;
+    int server;
+    int yes = 1;
+
+    struct sockaddr_in addr;
+
+
+    if (argc != 3)
     {
-    for ( ; *from != '\0'; ++to, ++from )
-	{
-	if ( from[0] == '%' && isxdigit( from[1] ) && isxdigit( from[2] ) )
-	    {
-	    *to = hexit( from[1] ) * 16 + hexit( from[2] );
-	    from += 2;
-	    }
-	else
-	    *to = *from;
-	}
-    *to = '\0';
+        fprintf(
+            stderr,
+            "usage: %s PORT DIRECTORY\n",
+            argv[0]
+        );
+
+        return 1;
     }
 
 
-static int
-hexit( char c )
+    port = atoi(argv[1]);
+
+
+    signal(
+        SIGCHLD,
+        SIG_IGN
+    );
+
+
+    server = socket(
+        AF_INET,
+        SOCK_STREAM,
+        0
+    );
+
+
+    if (server < 0)
     {
-    if ( c >= '0' && c <= '9' )
-	return c - '0';
-    if ( c >= 'a' && c <= 'f' )
-	return c - 'a' + 10;
-    if ( c >= 'A' && c <= 'F' )
-	return c - 'A' + 10;
-    return 0;		/* shouldn't happen, we're guarded by isxdigit() */
+        perror("socket");
+        return 1;
     }
 
 
-static void
-strencode( char* to, size_t tosize, const char* from )
+    setsockopt(
+        server,
+        SOL_SOCKET,
+        SO_REUSEADDR,
+        &yes,
+        sizeof(yes)
+    );
+
+
+    memset(
+        &addr,
+        0,
+        sizeof(addr)
+    );
+
+
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(port);
+
+
+    if (bind(
+            server,
+            (struct sockaddr *)&addr,
+            sizeof(addr)
+        ) < 0)
     {
-    int tolen;
-
-    for ( tolen = 0; *from != '\0' && tolen + 4 < tosize; ++from )
-	{
-	if ( isalnum(*from) || strchr( "/_.-~", *from ) != (char*) 0 )
-	    {
-	    *to = *from;
-	    ++to;
-	    ++tolen;
-	    }
-	else
-	    {
-	    (void) sprintf( to, "%%%02x", (int) *from & 0xff );
-	    to += 3;
-	    tolen += 3;
-	    }
-	}
-    *to = '\0';
+        perror("bind");
+        return 1;
     }
+
+
+    if (listen(server, 8) < 0)
+    {
+        perror("listen");
+        return 1;
+    }
+
+
+    printf(
+        "HTTP server listening on port %d\n",
+        port
+    );
+
+
+    while (1)
+    {
+        int client;
+
+
+        client = accept(
+            server,
+            NULL,
+            NULL
+        );
+
+
+        if (client < 0)
+            continue;
+
+
+
+        if (fork() == 0)
+        {
+            close(server);
+
+            handle_client(
+                client,
+                argv[2]
+            );
+
+            close(client);
+
+            exit(0);
+        }
+
+
+        close(client);
+    }
+
+
+    return 0;
+}
